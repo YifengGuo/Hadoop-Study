831b3b27334979d16cdd7a3781175deec97ebf86
Yifeng Guo <gyf940517@gmail.com>
2018-02-05 22:20:34 -0500

    update

--- src/word_count/WordCount1.java
+++ src/word_count/WordCount1.java
@@ -14,7 +14,7 @@ import java.io.IOException;
 
 public class WordCount1 {
     public static class WordCountMapper extends Mapper<Object, Text, Text, IntWritable> {
-        private final static IntWritable one = new IntWritable();
+        private final static IntWritable one = new IntWritable(1);
         private Text word = new Text();
 
         public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
