4c03422dc714e47ccd7ae356e4786f62ad6d59b2
Yifeng Guo <gyf940517@gmail.com>
2018-02-05 23:46:39 -0500

    add comment

--- src/word_count/WordCount1.java
+++ src/word_count/WordCount1.java
@@ -14,20 +14,40 @@ import java.io.IOException;
 
 public class WordCount1 {
     public static class WordCountMapper extends Mapper<Object, Text, Text, IntWritable> {
+        // IntWritable object one is initialized as static variable so we can reuse it to set the word in mapper
         private final static IntWritable one = new IntWritable(1);
+        // only initialize Text object word once and each time when it is called by function, reset it
+        // the idea in initializing the IntWritable and Text object is to save the resource and relieve the impact of GC
         private Text word = new Text();
 
+        /**
+         * the output of mapper is a sequence of pairs (word, 1), word is the key and 1 is the value
+         * the output will not be transmitted to Reducer immediately but need go through a process called shuffle
+         * @param key by default is the line number of text but does not matter in word_count
+         * @param value the content of text in a line
+         * @param context
+         * @throws IOException
+         * @throws InterruptedException
+         */
         public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
             String[] words = value.toString().split(" ");
 
             for ( String str : words) {
                 word.set(str);
-                context.write(word, one);
+                context.write(word, one); // context.write will do no change to Object one
             }
         }
     }
 
     public static class WordCountReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+        /**
+         * the output of Reducer is a sequence of pairs (word, count)
+         * @param key key (word) after shuffle (calculating hashCode)
+         * @param values the count
+         * @param context result pair for each word
+         * @throws IOException
+         * @throws InterruptedException
+         */
         public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
             int total = 0;
             for (IntWritable val : values) {
