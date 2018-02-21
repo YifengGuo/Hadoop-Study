7c063ab74fc687549ade3e8819bdb5fe72667026
Yifeng Guo <gyf940517@gmail.com>
2018-02-20 22:17:27 -0500

    update

--- src/ufo_statistic/shapetimemapper.py
+++ src/ufo_statistic/shapetimemapper.py
@@ -3,7 +3,7 @@ import re
 import sys
 
 # durations are described by min and sec
-pattern = re.compile(r'(\d*) ?((sec)|(min))?')
+pattern = re.compile(r'(\d*)(\s)?((sec)|(min))?')
 for line in sys.stdin:
     data = line.split("\t")
     if len(data) == 6:
@@ -11,11 +11,11 @@ for line in sys.stdin:
         duration = data[4].strip().lower()
         if len(shape) != 0 and len(duration) != 0:
             match_duration = re.search(pattern, duration)
-            time = re.compile(r'\d*').match(match_duration.group(1))  # match() return Match object, res[0] is digit
-            unit = match_duration.group(1)
-            if time.group(0).strip() == "":
+            time = re.compile(r'(\s?)(\d*)(\s?)').match(match_duration.group(1))  # match() return Match object, res[0] is digit
+            unit = match_duration.group(3)
+            if time.group(2).strip() == "":
                 continue
-            time = int(time.group(0).strip())
+            time = int(time.group(2).strip())
             if unit == "min":
                 time = time * 60
             print(shape + "\t" + str(time))
